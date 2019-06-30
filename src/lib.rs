617b8a0debe5641fa8a9a6309b73733b2fd277af
Anton Österberg <anton.osterberg@gmail.com>
2019-06-30 23:50:38 +0200

    Actually read args from argument vector

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+use serde_json::Value;
+use std::fmt;
+use std::cmp::Ordering;
+use chrono::DateTime;
+
+mod issue {
+    #[derive(Debug)]
+    struct Issue {
+        url: String,
+        title: String,
+        labels: Vec<String>,
+        comments: u32,
+        state: Option<State>,
+        updated_at: DateTime<chrono::FixedOffset>,
+        repository: String,
+    }
+
+    impl Issue {
+        fn from_json(data: &Value) -> Vec<Issue> {
+            let nodes: Option<&Vec<Value>> = data["data"]["search"]["edges"].as_array();
+            let empty = Vec::new();
+            let nodes: &Vec<Value> = match nodes {
+                Some(vec) => vec,
+                None => &empty
+            };
+            let mut issues: Vec<Issue> = nodes.iter()
+                .filter(|node| node.is_object())
+                .map(|node| node.get("node"))
+                .filter(|node| node.is_some())
+                .map(|node| match node {
+                    Some(n) => n,
+                    None => panic!("Impossible")
+                })
+                .map(|node| Issue::from_node(&node))
+                .filter(|node| node.is_some())
+                .map(|node| node.expect("This is ok"))
+                .collect::<Vec<Issue>>();
+
+            issues.sort_by(|issue1, issue2| if issue2.comments < issue1.comments { Ordering::Less } else { Ordering::Greater});
+            issues
+        }
+
+        fn from_node(node: &Value) -> Option<Issue> {
+            let comments: Option<u64> = node["comments"]["totalCount"].as_u64();
+            let comments: u32 = match comments {
+                Some(c) => c as u32,
+                None => 0u32
+            };
+            let updated_at: &str = match node["updatedAt"].as_str() {
+                Some(s) => s,
+                None => return None
+            };
+            let issue = Issue {
+                url: node["url"].as_str()?.to_string(),
+                title: node["title"].as_str()?.to_string(),
+                labels: Vec::new(),
+                state: State::from_string(&node["state"].as_str()?.to_string()),
+                comments,
+                updated_at: DateTime::parse_from_rfc3339(updated_at).expect("Unable to parse date"),
+                repository: node["repository"]["nameWithOwner"].as_str()?.to_string()
+            };
+            Some(issue)
+        }
+    }
+
+    impl fmt::Display for Issue {
+        fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+            fmt.write_fmt(format_args!("{} -> {}\t[{}]", &self.repository, &self.title, &self.url));
+            Ok(())
+        }
+    }
+
+    impl Ord for Issue {
+        fn cmp(&self, other: &Self) -> Ordering {
+            if self.comments != other.comments {
+                return self.comments.cmp(&other.comments)
+            }
+            self.updated_at.cmp(&other.updated_at)
+        }
+    }
+
+    impl PartialOrd for Issue {
+        fn partial_cmp(&self, other: &Issue) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl Eq for Issue {}
+
+    impl PartialEq for Issue {
+        fn eq(&self, other: &Issue) -> bool {
+            self.url == other.url
+                && self.comments == other.comments
+                && self.updated_at == other.updated_at
+        }
+    }
+}
