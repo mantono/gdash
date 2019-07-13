9dcbce306a9e81fbf29de770f683afd55a71ecb7
Anton Österberg <anton.osterberg@gmail.com>
2019-07-13 23:29:50 +0200

    Properly support viewing pull requests and review requests

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use gdash::issue::issue::Issue;
 use gdash::args::Arguments;
 use std::env;
 use serde_json::Value;
-use std::collections::HashMap;
+use serde_json::json;
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let args = Arguments::from_args();
@@ -35,53 +35,23 @@ fn main() -> Result<(), Box<std::error::Error>> {
     };
 
     let client: reqwest::Client = reqwest::Client::new();
-    let issues: Vec<Issue> = api_request(&client, issues, token).expect("Could not request issues");
+    let mut issues: Vec<Issue> = api_request(&client, issues, &token).expect("Could not request issues");
+    let mut pull_requests: Vec<Issue> = api_request(&client, pull_requests, &token).expect("Could not request pull requests");
+    let mut review_requests: Vec<Issue> = api_request(&client, review_requests, &token).expect("Could not request review requests");
 
-    for i in issues {
+    let mut all: Vec<Issue> = Vec::with_capacity(64);
+    all.append(&mut issues);
+    all.append(&mut pull_requests);
+    all.append(&mut review_requests);
+    all.sort();
+
+    for i in all {
         println!("{}", i)
     };
     Ok(())
-
-/*     let issues: GraphQL = GraphQL::build_from(issues);
-    let pull_requests: GraphQL = GraphQL::build_from(pull_requests);
-
-    println!("{:?}", issues);
-
-    let mut json_data: HashMap<String, String> = HashMap::new();
-    let query = include_str!("search_issues.graphql").to_string();
-
-    let mut variables: String = r#"{ "searchQuery": "is:open archived:false assignee:"#.to_string();
-    variables.push_str(&args.user.clone());
-    for org in args.orgs.iter() {
-        variables.push_str(&format!(" user:{}", &org));
-    }
-    variables.push_str(r#" sort:comments-desc" }"#);
-    json_data.insert(String::from("query"), query);
-    json_data.insert(String::from("variables"), String::from(variables));
-    json_data.insert(String::from("operationName"), String::from("UserIssues"));
-
-    let client: reqwest::Client = reqwest::Client::new();
-    let response: String = client
-        .post("https://api.github.com/graphql")
-        .bearer_auth(token)
-        .json(&json_data)
-        .send()?
-        .text()?;
-
-    let json: Value = serde_json::from_str(&response)?;
-    let issues: Vec<Issue> = Issue::from_json(&json); */
-
-    
-    // for i in issues {
-    //     println!("{}", i)
-    // }
-    // for p in pull_requests {
-    //     println!("{}", p)
-    // }
-    // Ok(())
 }
 
-fn api_request(client: &reqwest::Client, query: QueryType, token: String) -> Result<Vec<Issue>, &'static str> {
+fn api_request(client: &reqwest::Client, query: QueryType, token: &String) -> Result<Vec<Issue>, &'static str> {
     let gql = GraphQL::build_from(query);
     let json = gql.as_json();
     let response: Result<reqwest::Response, reqwest::Error> = client
@@ -90,13 +60,11 @@ fn api_request(client: &reqwest::Client, query: QueryType, token: String) -> Res
         .json(&json)
         .send();
 
-    println!("{:?}", json);
     let body: String = match response {
         Ok(mut data) => data.text().expect("Unable to read body"),
         Err(err) => panic!(err)
     };
 
-    println!("{}", body);
     let json: Value = serde_json::from_str(body.as_str()).expect("Cannot parse response as JSON");
     Ok(Issue::from_json(&json))
 }
@@ -117,12 +85,13 @@ impl GraphQL {
         }
     }
 
-    fn as_json(&self) -> HashMap<String, String> {
-        let mut json: HashMap<String, String> = HashMap::new();
-        json.insert(String::from("query"), self.query.clone());
-        json.insert(String::from("searchQuery"), self.search_query.clone());
-        json.insert(String::from("operationName"), self.operation_name.clone());
-        json
+    fn as_json(&self) -> Value {
+        json!({
+            "query": self.query.clone(),
+            "variables": {
+                "searchQuery": self.search_query.clone()
+            }
+        })
     }
 }
 
@@ -186,7 +155,6 @@ impl QueryType {
         let additional: String = format_args!("{}{}", additional, self.user().clone()).to_string();
         let users: String = self.orgs().iter().map(|o| format!("user:{}", o)).collect::<Vec<String>>().join(" ");
         let query: String = vec![base_query, additional, users].join(" ");
-        println!("{}", query);
         query
     }
 }
